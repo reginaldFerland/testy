b616862333ef2ce0965bc5a62317fa15717fb3ce
Reginald Ferland <reginald@ferland.io>
2025-05-23 10:44:55 -0400

    Detecting file changes

--- src/extension.ts
+++ src/extension.ts
@@ -20,7 +20,31 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+
+	// Set up file system watcher for .cs files
+	const csWatcher = vscode.workspace.createFileSystemWatcher('**/*.cs');
+
+	// Watch for file created events
+	csWatcher.onDidCreate(uri => {
+		console.log(`CS file created: ${uri.fsPath}`);
+		vscode.window.showInformationMessage(`CS file created: ${uri.fsPath}`);
+	});
+
+	// Watch for file changed events
+	csWatcher.onDidChange(uri => {
+		console.log(`CS file changed: ${uri.fsPath}`);
+		vscode.window.showInformationMessage(`CS file changed: ${uri.fsPath}`);
+	});
+
+	// Watch for file deleted events
+	csWatcher.onDidDelete(uri => {
+		console.log(`CS file deleted: ${uri.fsPath}`);
+		vscode.window.showInformationMessage(`CS file deleted: ${uri.fsPath}`);
+	});
+
+	// Make sure to dispose the watcher when the extension is deactivated
+	context.subscriptions.push(csWatcher);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
